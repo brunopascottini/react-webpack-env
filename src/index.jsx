0579e1f85b8aa0de19e3bd03d9f7b0608cd1d140
brunopascottini <bruno.pascottini@gmail.com>
2021-07-03 15:39:10 +0100

    Add normalize.css

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routes'
+import './normalize.css'
 
 ReactDOM.render(<AppRouter />, document.getElementById('index'))
