019d39f5809eca2976918598bc54e5ebb416045f
brunopascottini <bruno.pascottini@gmail.com>
2021-07-04 12:41:02 +0100

    Add Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    trailingComma: 'es5',
+    singleQuote: true,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    tabs: true,
+    endOfLine: 'crlf',
+}
